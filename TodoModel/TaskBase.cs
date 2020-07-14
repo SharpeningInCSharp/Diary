ed3acd0299bd1967f3805532a008de4abb4d2a50
Fedor Ishchenko <aleks180700@gmail.com>
2020-07-14 12:55:04 +1000

    Interface implementation

--- TodoModel/TaskBase.cs
+++ TodoModel/TaskBase.cs
@@ -4,16 +4,35 @@ namespace Model
 {
 	public abstract class TaskBase : IEquatable<TaskBase>
 	{
+		public bool IsCompleted { get; private set; } = false;
+
 		public string Header { get; set; }
 
 		public string Note { get; set; }
 
 		public IPriority Priority { get; set; }
 
-		public abstract void Complete();
+		/// <summary>
+		/// Completes task
+		/// </summary>
+		public void Complete()
+		{
+			IsCompleted = true;
+		}
 
+		public void Undo()
+		{
+			IsCompleted = false;
+		}
+
+		/// <summary>
+		/// Deletes task
+		/// </summary>
 		public abstract void Delete();
 
+		/// <summary>
+		/// Delays task
+		/// </summary>
 		public abstract void SetAside();
 
 		public bool Equals(TaskBase other)
