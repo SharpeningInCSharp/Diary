bda679a543c43813a90cf1c05c0263d6a0b4b947
Seleznev Dmitry <2000.Seleznyov51@gmail,com>
2020-07-17 00:23:55 +1000

    Reaml classes

---
+++ TodoModel/Database/PriorityEntity.cs
@@ -0,0 +1,16 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TodoModel.Database
+{
+    //class PriorityEntity : RealmObject
+    //{
+    //    [PrimaryKey]
+    //    string Name { get; set; }
+    //    int Value { get; set; }
+    //    int Color { get; set; }
+    //}
+}
---
+++ TodoModel/Database/TodoNote.cs
@@ -0,0 +1,18 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoModel.Database
+{
+    //class TodoNote : RealmObject
+    //{
+       
+    //    [PrimaryKey]
+    //    public int Id { get; set; }
+    //    public string header { get; set; }
+    //    public bool IsCompleted { get; set; }
+
+
+    //}
+}
