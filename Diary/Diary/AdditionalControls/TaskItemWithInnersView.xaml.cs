6c34c2bc9647c7e040ef69b0ebb4f732860b3c70
Fedor Ishchenko <aleks180700@gmail.com>
2020-07-17 19:52:27 +1000

    Before Brunch

--- Diary/Diary/AdditionalControls/TaskItemWithInnersView.xaml.cs
+++ Diary/Diary/AdditionalControls/TaskItemWithInnersView.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using TodoModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +7,13 @@ namespace Diary.AdditionalControls
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class TaskItemWithInnersView : ContentView
 	{
+		public Task Task { get; set; }
+
 		public TaskItemWithInnersView()
 		{
 			InitializeComponent();
+
+			BindingContext = MainItem.Task = Task;
 		}
 	}
 }
