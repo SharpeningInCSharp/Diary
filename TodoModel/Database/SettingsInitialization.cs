69dbf3b972654391b45fe278955cedc570fa8a0a
Seleznev Dmitry <2000.Seleznyov51@gmail,com>
2020-07-19 19:19:53 +1000

    Топ коммит

--- TodoModel/Database/SettingsInitialization.cs
+++ TodoModel/Database/SettingsInitialization.cs
@@ -1,4 +1,5 @@
-﻿using Realms;
+﻿using Android.Util;
+using Realms;
 using Remotion.Linq.Parsing.Structure.IntermediateModel;
 using System;
 using System.Collections.Generic;
@@ -9,25 +10,35 @@ namespace TodoModel.Database
 {
     public class SettingsInitialization
     {
-        public static void ParamsSetting()
+        public static int ParamsSetting()
         {
             var realm = Realm.GetInstance();
             var starts = realm.All<Settings>().Where(p => p.Param == "Starts").FirstOrDefault();
-            if(starts == null)
+            if (starts == null)
+            {
                 realm.Write(() =>
                 {
                     var newSet = new Settings
                     {
-                       Param = "Starts",
-                       //value = 1
+                        Param = "Starts",
+                        value = 1
                     };
+                    //Log.Error("1111111111111111111111111", "22222222222222222222222222222222222222222");
+                    //Console.WriteLine("555555555555555555555555555555555555555555");
                     realm.Add(newSet);
                 });
+                return 12;
+            }
             else
+            {
                 realm.Write(() =>
                 {
-                    //starts.value++;
+                    //Log.Error("222222222222222222222222222222222222", "33333333333333333333333333333333333333333333333");
+                    //Console.WriteLine("444444444444444444444444444444444444444444");
+                    starts.value = starts.value + 1;
                 });
+                return 13;
+            }
         }
     }
 }
