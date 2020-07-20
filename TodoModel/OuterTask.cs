954ffc762128a4d566319b7891abb3fa5630384d
Fedor Ishchenko <aleks180700@gmail.com>
2020-07-20 21:16:43 +1000

    OuterTask Collection update
    IT DOESN'T WORK AGAIN ON MINE

--- TodoModel/OuterTask.cs
+++ TodoModel/OuterTask.cs
@@ -8,12 +8,12 @@ namespace TodoModel
 {
 	public partial class OuterTask : TaskBase
 	{
-		private readonly List<Task> innerTasks = new List<Task>();
+		private readonly List<TaskBase> innerTasks = new List<TaskBase>();
 
 		/// <summary>
 		/// This prop return only values, DON'T use it to change object!
 		/// </summary>
-		public IEnumerable<Task> InnerTasks => new List<Task>(innerTasks);
+		public IEnumerable<TaskBase> InnerTasks => new List<TaskBase>(innerTasks);
 
 		public DateTime? InitialDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 		public DateTime? FinalDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
@@ -23,7 +23,24 @@ namespace TodoModel
 
 		public void Add(Task task)
 		{
+			task.TaskCompleted += Task_TaskCompleted;
+			task.TaskDeleted += Task_TaskDeleted;
+
 			innerTasks.Add(task ?? throw new ArgumentNullException(nameof(task)));
+
+			OnPropertyChanged("InnerTasks");
+		}
+
+		private void Task_TaskDeleted(TaskBase task)
+		{
+			innerTasks.Remove(task);
+
+			OnPropertyChanged("InnerTasks");
+		}
+
+		private void Task_TaskCompleted(TaskBase task)
+		{
+			innerTasks.Remove(task);
 
 			OnPropertyChanged("InnerTasks");
 		}
