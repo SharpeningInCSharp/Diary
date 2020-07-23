a07fb4d78ce59eea046e2e26bd23bd20e2df8cf3
Seleznev Dmitry <2000.Seleznyov51@gmail,com>
2020-07-23 18:52:29 +1000

    НЕ ВЫТЯГИВАТЬ

--- TodoModel/Database/SettingsInitialization.cs
+++ TodoModel/Database/SettingsInitialization.cs
@@ -4,18 +4,51 @@ using Remotion.Linq.Parsing.Structure.IntermediateModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace TodoModel.Database
 {
     public class SettingsInitialization
     {
-        public static int ParamsSetting()
+        public static void ParamsSetting()
         {
-            var realm = Realm.GetInstance();
+            //var realmm = Realm.GetInstance();
+            //realmm.Write(() =>
+            //{
+            //    var newSet = new TaskListEntity
+            //    {
+            //        Name = "Main"
+            //    };
+            //    realmm.Add(newSet);
+            //});
+
+            //var config = new RealmConfiguration
+            //{
+            //    SchemaVersion = 3,
+            //    MigrationCallback = (migration, oldSchemaVersion) =>
+            //    {
+            //        var newTasks = migration.NewRealm.All<TodoNote>();
+
+            //        // Use the dynamic api for oldPeople so we can access
+            //        // .FirstName and .LastName even though they no longer
+            //        // exist in the class definition.
+
+            //        for (var i = 0; i < newTasks.Count(); i++)
+            //        {
+            //            newTasks.ElementAt(i).taskList = null;
+            //        }
+            //    }
+            //};
+
+            //var realm = Realm.GetInstance(config);
+
+            var config = new RealmConfiguration() { SchemaVersion = 3 };
+
+            var realm = Realm.GetInstance(config);
+
             var starts = realm.All<Settings>().Where(p => p.Param == "Starts").FirstOrDefault();
             if (starts == null)
-            {
                 realm.Write(() =>
                 {
                     var newSet = new Settings
@@ -23,22 +56,13 @@ namespace TodoModel.Database
                         Param = "Starts",
                         value = 1
                     };
-                    //Log.Error("1111111111111111111111111", "22222222222222222222222222222222222222222");
-                    //Console.WriteLine("555555555555555555555555555555555555555555");
                     realm.Add(newSet);
                 });
-                return 12;
-            }
             else
-            {
                 realm.Write(() =>
                 {
-                    //Log.Error("222222222222222222222222222222222222", "33333333333333333333333333333333333333333333333");
-                    //Console.WriteLine("444444444444444444444444444444444444444444");
-                    starts.value = starts.value + 1;
+                    starts.value += 1;
                 });
-                return 13;
-            }
         }
     }
 }
---
+++ TodoModel/Database/TaskListEntity.cs
@@ -0,0 +1,14 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoModel.Database
+{
+    public class TaskListEntity : RealmObject
+    {
+        [PrimaryKey]
+        public string Name { get; set; }
+        public IList<TodoNote> notes { get; }
+    }
+}
