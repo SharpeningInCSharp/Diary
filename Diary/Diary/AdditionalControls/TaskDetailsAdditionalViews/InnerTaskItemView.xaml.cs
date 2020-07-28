0b7062ff957fe99eb8999846db1145dde39b7a2b
Fedor Ishchenko <aleks180700@gmail.com>
2020-07-28 18:42:31 +1000

    GUI item addition

---
+++ Diary/Diary/AdditionalControls/TaskDetailsAdditionalViews/InnerTaskItemView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TodoModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Diary.AdditionalControls.TaskDetailsAdditionalViews
+{
+	/// <summary>
+	/// This item's used for displayng and editing <see cref="TaskBase"/> title, its completing and deleting.
+	/// It doesn't supposed view Task details
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InnerTaskItemView : ContentView
+	{
+		private readonly TaskBase innerTask;
+		public InnerTaskItemView(TaskBase innerTask)
+		{
+			InitializeComponent();
+
+			this.innerTask = innerTask;
+		}
+
+		private void CrossButton_Clicked(object sender, EventArgs e)
+		{
+			innerTask.Delete();
+		}
+	}
+}
